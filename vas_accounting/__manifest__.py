f0ae53d250a216e3aa6159a6aa5dcdb17d0a8121
RoPho <ropho164@gmail.com>
2026-08-10 19:55:17 +0700

    feat: add cover image and update manifest (images field, website url)

--- vas_accounting/__manifest__.py
+++ vas_accounting/__manifest__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'VAS - Vietnamese Accounting Standards',
-    'icon': '/vas_accounting/static/description/icon.png',
+    'icon': '/vas_accounting_public/static/description/icon.png',
     'version': '19.0.1.0.0',
     'category': 'Accounting/Localizations',
     'summary': 'VAS - Kế toán Việt Nam | Giải pháp kế toán tích hợp theo chuẩn mực kế toán Việt Nam (VAS).',
     'author': 'Hub2S Vietnam Co., Ltd',
-    'website': 'https://hub2s.com',
+    'website': 'https://hub2s.com/giai-phap-vas',
     'license': 'OPL-1',
     'depends': [
         'stock',
@@ -23,9 +23,12 @@
     ],
     'assets': {
         'web.assets_backend': [
-            'vas_accounting/static/src/js/vas_payment_required.js',
+            'vas_accounting_public/static/src/js/vas_payment_required.js',
         ],
     },
+    'images': [
+        'vas_accounting_public/static/description/cover.png',
+    ],
     'installable': True,
     'auto_install': False,
     'application': True,
